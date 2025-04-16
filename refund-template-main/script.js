dbc2d3bbf60b208bd5fb9c31ebc3f9dfe51c102d
claudio.te <claudio.souza.te@gmail.com>
2025-04-16 15:01:09 -0300

    percorrendo os itens da lista e calculando o valor total.

--- refund-template-main/script.js
+++ refund-template-main/script.js
@@ -5,6 +5,9 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
+const expensesQuantity = document.querySelector("aside header p span")
+
 
 amount.oninput = () => {
 let value = amount.value.replace(/\D/g, "")
@@ -74,13 +77,48 @@ function expenseAdd(newExpense) {
       removeIcon.setAttribute("alt", "remover")
 
       expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+      
       expenseList.append(expenseItem)
 
+     updateTotals()
    } catch (error) {
      alert("Não foi possível atualizar a lista de despesas.")
     console.log(error)
 
     expenseItem.append(expenseIcon)
    }
+}
+
+function updateTotals() {
+  try {
+    
+    const items = expenseList.children
+   
+    expensesQuantity.textContent = `${items.length} ${ items.length > 1 ? "despesas" : "despesa" }`
+
+    let total = 0
+
+    for(let item = 0; item < items.length; item++){
+      const itemAmount = items[item].querySelector(".expense-amount")
+
+      let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",",".")
+
+      value = parseFloat(value)
+
+      if (isNaN(value)) {
+        return alert(
+          "Não foi possível calcular o total. O valor não parece ser um número."
+        )
+      }
+
+      total += Number(value)
+    }
+  
+    expensesTotal.textContent = total 
+
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível atualizar os totais.")
+  }
 }
 
